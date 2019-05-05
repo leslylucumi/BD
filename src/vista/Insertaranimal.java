1d94c0e5a1711363e93b6feb3e4420503c50e504
angela77777 <49988800+angela77777@users.noreply.github.com>
2019-05-04 20:02:47 -0500

    otra vez viajes

--- src/vista/Insertaranimal.java
+++ src/vista/Insertaranimal.java
@@ -13,12 +13,12 @@ import modelo.Especie;
  *
  * @author Acer
  */
-public class InsertarAnimal extends javax.swing.JFrame {
+public class Insertaranimal extends javax.swing.JFrame {
 
     /**
      * Creates new form InsertarAnimal
      */
-    public InsertarAnimal() {
+    public Insertaranimal() {
         initComponents();
         
         
@@ -146,20 +146,21 @@ public class InsertarAnimal extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(InsertarAnimal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Insertaranimal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(InsertarAnimal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Insertaranimal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(InsertarAnimal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Insertaranimal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(InsertarAnimal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Insertaranimal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new InsertarAnimal().setVisible(true);
+                new Insertaranimal().setVisible(true);
             }
         });
     }
